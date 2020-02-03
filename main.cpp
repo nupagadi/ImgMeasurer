b1524e3a128daa20bc5646702968e51456126b25
Ilya <mail@mail.com>
2020-02-03 23:34:51 +0300

    help and debug config params.

--- main.cpp
+++ main.cpp
@@ -136,12 +136,14 @@ float CalcDistance(const Mat& aHomography,
 
 struct Config
 {
-    enum Mode {Error, Normal, Debug};
+    enum Mode {Error, Normal, Debug, Help};
 
     Config::Mode Mode;
     std::string FileName;
-    float LaneWidth;
+    float LaneWidth {}
     std::pair<int,int> Point1, Point2;
+
+    bool IsGuiDebug = false;
 
     operator bool() const
     {
@@ -155,10 +157,29 @@ Config MakeConfig(const std::vector<std::string>& aArgs)
     Config cfg;
     cfg.Mode = Config::Error;
 
+    auto it = std::find(args.cbegin(), args.cend(), "--help");
+    if (it != args.cend())
+    {
+        cfg.Mode = Config::Help;
+        return cfg;
+    }
+
+    it = std::find(args.cbegin(), args.cend(), "--debug");
+    if (it != args.cend())
+    {
+        it = args.erase(it);
+        cfg.Mode = Config::Debug;
+        if (it != args.cend() && *it == "gui")
+        {
+            it = args.erase(it);
+            cfg.IsGuiDebug = true;
+        }
+    }
+
     if (args.size() < 10)
         return cfg;
 
-    auto it = std::find(args.cbegin(), args.cend(), "--file");
+    it = std::find(args.cbegin(), args.cend(), "--file");
     if (it == args.cend() || ++it == args.cend())
         return cfg;
     cfg.FileName = *it;
@@ -190,6 +211,9 @@ void PrintHelp()
     std::cout << "Usage:\n";
     std::cout << "./ImgMeasurer --file /path/to/file.png --lane 3.75 "
        << "--point1 <x> <y> --point2 <x> <y>" << std::endl;
+    std::cout << std::endl << "Optional parameters:\n";
+    std::cout << "--debug [gui]" << std::endl;
+    std::cout << "Show additional info. \"gui\" - for graphical representation." << std::endl;
 }
 
 void PrintConfig(const Config& aConfig)
@@ -197,6 +221,11 @@ void PrintConfig(const Config& aConfig)
     if (!aConfig)
     {
         std::cout << "Invalid config" << std::endl;
+        PrintHelp();
+        return;
+    }
+    if (aConfig.Mode == Config::Help)
+    {
         PrintHelp();
         return;
     }
@@ -216,6 +245,10 @@ int main(int argc, char** argv)
     if (!config)
     {
         return -1;
+    }
+    if (config.Mode == Config::Help)
+    {
+        return 0;
     }
 
     Mat image;
